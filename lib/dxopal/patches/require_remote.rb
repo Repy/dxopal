59ecd5134a997e55fa8e23d6e1600a366d855201
terasaka.reiji <terasaka.reiji@blue.ibk.ed.jp>
2024-07-13 05:18:02 +0000

    Allow source-map to be used with require_remote

--- lib/dxopal/patches/require_remote.rb
+++ lib/dxopal/patches/require_remote.rb
@@ -1,10 +1,12 @@
 require 'opal-parser'
-def require_remote(url)
-  %x{
-    var r = new XMLHttpRequest();
-    r.overrideMimeType("text/plain"); // https://github.com/yhara/dxopal/issues/12
-    r.open("GET", url, false);
-    r.send('');
-  }
-  eval `r.responseText`
+module Kernel
+  def require_remote(url)
+    %x{
+      var r = new XMLHttpRequest();
+      r.overrideMimeType("text/plain"); // https://github.com/yhara/dxopal/issues/12
+      r.open("GET", encodeURI(url), false);
+      r.send('');
+    }
+    Kernel.eval(`r.responseText`,nil,`encodeURI(url)`)
+  end
 end
